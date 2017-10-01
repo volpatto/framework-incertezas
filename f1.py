32db6a0b0b165dc4c052d415422801b40fe6a075
volps <volpatto@lncc.br>
2017-10-01 13:52:34 -0300

    Cria diretorio para rotinas de pos-processamento

--- f1.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-x = np.linspace(0, 100, 50) 
-y1 = -(x-50)**2 + 2500
-y2 = (200*x-50)
-plt.plot(x,y1)
-plt.plot(x,y2)
-plt.fill_between(x,y1, y2, color='green')
-plt.show()
